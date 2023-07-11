3c32a61b076c5d2d3fa4fc2d87648b25cdbf80fa
Stephen Doty <stephendoty826@yahoo.com>
2023-07-11 11:42:51 -0500

    added action and method to Form tag and name to Form.control tag for email and password. Once this was done, the form worked as intended and user email and password was entered into database

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,23 +3,24 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
 const Register = () => {
+
   return (
     <div className="container">
       <div className="row">
         <p className="display-5 text-center">Register Here</p>
         <div className="col-12 d-flex justify-content-center">
-          <Form className="col-3" action="/register" method="POST">
+          <Form action="/register" method="POST" className="col-5">
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control type="email" placeholder="Enter email" name="email" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control type="password" placeholder="Password" name="password"/>
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
               <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control type="password" placeholder="Confirm Password" />
             </Form.Group>
             <Button variant="primary" type="submit">
               Submit
