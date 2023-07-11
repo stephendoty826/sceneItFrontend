4747df6a5490e9c5e03a0f4e285888512aeeff64
Stephen Doty <stephendoty826@yahoo.com>
2023-07-11 11:25:17 -0500

    Added react form to Register component and set up proxy in package.json file

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const Register = () => {
   return (
     <div className="container">
       <div className="row">
+        <p className="display-5 text-center">Register Here</p>
         <div className="col-12 d-flex justify-content-center">
-          Register Form Here
+          <Form className="col-3" action="/register" method="POST">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </div>
       </div>
     </div>
+
+    
   )
 }
 
