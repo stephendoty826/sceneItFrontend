89a457f2bce67f3b0dd1710e8f49fe3a12d570d2
Stephen Doty <stephendoty826@yahoo.com>
2023-09-29 14:08:49 -0500

    working to setup express-session and connect-session-sequelize. I can't test it without add the code for passport and logging in a user. Next will be working to get that code in.

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,25 @@
 const express = require('express');
-const helmet = require('helmet')
-const cookieSession = require('cookie-session')
+
+const session = require('express-session')
+const Sequelize = require("sequelize")
+// initialze sequelize with session store
+const SequelizeStore = require("connect-session-sequelize")(session.Store)
+
 const passport = require('passport')
 require('./auth/passportConfig')(passport)
 
-const app = express();
+const helmet = require('helmet')
+
 const port = 3050;
+
+/**
+ * -------------- GENERAL SETUP ----------------
+ */
+
+// Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
+require('dotenv').config();
+
+const app = express();
 
 app.use(helmet());
 
@@ -13,11 +27,44 @@ app.use(helmet());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-app.use(cookieSession({
-  name: 'session', 
-  keys: ['keyforCookie'],
-  maxAge: 14 * 24 * 60 * 60 * 1000 // 2 weeks
+/**
+ * ----------------- SESSION SETUP -------------------
+ */
+
+// create database
+const sequelize = new Sequelize("sceneItFullStack", "postgres", null, {
+  dialect: 'postgres',
+  host: '127.0.0.1'
+})
+
+// Define a model for your session table
+const sessionData = sequelize.define("sessionData", {
+  sid: {
+    type: Sequelize.STRING,
+    primaryKey: true
+  },
+  data: Sequelize.TEXT,
+  expires: Sequelize.DATE,
+});
+
+const sessionStore = new SequelizeStore({
+  db: sequelize,
+  table: "sessionData",
+  //* I shouldn't have to extendDefaultFields since I am just using the default fields and not extending them
+})
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: sessionStore
 }))
+
+// so sessionStore will create/sync the db table
+sessionStore.sync(); 
+
+
+
 
 app.use(passport.initialize())
 app.use(passport.session())
@@ -25,6 +72,6 @@ app.use(passport.session())
 app.use(require('./routes/loginRoutes'))
 app.use(require('./routes/profileRoutes'))
 
-app.listen(port, ()=> {
+app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
