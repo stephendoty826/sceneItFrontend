ec296b7b526c9e8fce53c2f1668538cfb276f20c
Stephen Doty <stephendoty826@yahoo.com>
2023-10-07 13:08:55 -0500

    Made automatic fetch of watchlist conditional on whether or not user was logged in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,84 +9,95 @@ import Button from "react-bootstrap/Button"
 import axios from 'axios';
 
 function App() {
-
-  const [watchlistIds, setWatchlistIds] = useState([])
+  const [watchlistIds, setWatchlistIds] = useState([]);
   const [searchField, setSearchField] = useState("");
   const [movieArray, setMovieArray] = useState([]);
 
-  const apiKey = process.env.REACT_APP_API_KEY
+  const apiKey = process.env.REACT_APP_API_KEY;
 
   const firstName = useParams().firstName;
 
-  const firstNameInCookies = Cookies.get('firstName');
+  const firstNameInCookies = Cookies.get("firstName");
 
-  // sets firstName in cookies when user logs in 
-  if(!firstNameInCookies && firstName !== 'home'){
-    Cookies.set("firstName", firstName, {expires: 14});
+  // checks whether or not user is logged in
+  const userLoggedIn = !firstNameInCookies && firstName !== "home";
+
+  // sets firstName in cookies when user logs in
+  if (userLoggedIn) {
+    Cookies.set("firstName", firstName, { expires: 14 });
   }
 
   useEffect(() => {
     // fetch watchlist imdbIDs so that array can be passed to isMovieOnWatchlist function
-    fetch("/watchlist") //todo use thunk???
-    .then(response => {
-      return response.json()
-    })
-    .then(imdbIDArray => {
-      setWatchlistIds(imdbIDArray)
-    })
-  }, [])
+    if (userLoggedIn) {
+      fetch("/watchlist") //todo use thunk???
+        .then((response) => {
+          return response.json();
+        })
+        .then((imdbIDArray) => {
+          setWatchlistIds(imdbIDArray);
+        });
+    }
+  }, []);
 
   const fetchMovieData = (urlEncodedSearchField) => {
-    axios.get(`http://www.omdbapi.com/?apikey=${apiKey}&s=${urlEncodedSearchField}&page=1`)
-    .then(response => {
-      if(response.data.Response === "True"){
-        let responseMovieArray = response.data.Search.reduce((acc, movie) => {
-          if(movie.Poster !== "N/A" && movie.Type !== "game"){ // filters out those objects that don't have posters and are games
-            return [...acc, movie]
-          }
-          return acc
-        }, [])
-          let tempMovieArray = isMovieOnWatchlist(watchlistIds, responseMovieArray)
-          setMovieArray(tempMovieArray)
-      }
-    })
-  }
+    axios
+      .get(
+        `http://www.omdbapi.com/?apikey=${apiKey}&s=${urlEncodedSearchField}&page=1`
+      )
+      .then((response) => {
+        if (response.data.Response === "True") {
+          let responseMovieArray = response.data.Search.reduce((acc, movie) => {
+            if (movie.Poster !== "N/A" && movie.Type !== "game") {
+              // filters out those objects that don't have posters and are games
+              return [...acc, movie];
+            }
+            return acc;
+          }, []);
+          let tempMovieArray = isMovieOnWatchlist(
+            watchlistIds,
+            responseMovieArray
+          );
+          setMovieArray(tempMovieArray);
+        }
+      });
+  };
 
   /**
    *  function to see determine which movies in responseMovieArray are on the watchlist. If movie is on watchlist, set isOnWatchlist boolean on movie object to true, otherwise set it to false.
    * @watchlistIds array imdbIDs on watchlist
    * @responseMovieArray array of movie objects resulting from search
-   * */ 
+   * */
   function isMovieOnWatchlist(watchlistIds, responseMovieArray) {
-    let mappedMovieArray = responseMovieArray.map(movie => {
-      let onWatchlistFlag = watchlistIds.find((imdbIDObj) => imdbIDObj.imdbID === movie.imdbID)
-      if(onWatchlistFlag){
-        movie.onWatchlist = true
-        return movie
-      }else{
-        movie.onWatchlist = false
-        return movie
+    let mappedMovieArray = responseMovieArray.map((movie) => {
+      let onWatchlistFlag = watchlistIds.find(
+        (imdbIDObj) => imdbIDObj.imdbID === movie.imdbID
+      );
+      if (onWatchlistFlag) {
+        movie.onWatchlist = true;
+        return movie;
+      } else {
+        movie.onWatchlist = false;
+        return movie;
       }
-      
-    })
-    return mappedMovieArray
+    });
+    return mappedMovieArray;
   }
 
   // function to be passed down to MovieCard component
   function handleAddToWatchlistClick(imdbID) {
-
     // adds to local watchlist cache
-    setWatchlistIds([...watchlistIds, {imdbID}])
+    setWatchlistIds([...watchlistIds, { imdbID }]);
 
-    // fetch to add to db 
-    fetch(`/watchlist/${imdbID}`,{
+    // fetch to add to db
+    fetch(`/watchlist/${imdbID}`, {
       method: "POST",
       headers: {
-        "Content-Type": "applications/json"
-      }
+        "Content-Type": "applications/json",
+      },
     })
-    .then(response => response.json())
-    .then(response => console.log(response));
+      .then((response) => response.json())
+      .then((response) => console.log(response));
   }
 
   const AddBtnDetails = {
@@ -94,21 +105,28 @@ function App() {
     onClick: handleAddToWatchlistClick,
     text: "Add To Watchlist",
     type: "add",
-    variant: "primary"
-  }
-
+    variant: "primary",
+  };
 
   return (
     <>
       <div className="container">
         <div className="row">
           <div className="col-12 header text-center mb-3">
-            <h1 style={{color: "black"}}>Search movies or series</h1>
-            {firstNameInCookies ? <h4>Save them to your watchlist</h4> : <h4>Login to save them to your watchlist</h4>}
+            <h1 style={{ color: "black" }}>Search movies or series</h1>
+            {firstNameInCookies ? (
+              <h4>Save them to your watchlist</h4>
+            ) : (
+              <h4>Login to save them to your watchlist</h4>
+            )}
           </div>
         </div>
-        <SearchBar searchField={searchField} setSearchField={setSearchField} fetchMovieData={fetchMovieData}/>
-        <CardList btnDetails={AddBtnDetails} movieArray={movieArray}/>
+        <SearchBar
+          searchField={searchField}
+          setSearchField={setSearchField}
+          fetchMovieData={fetchMovieData}
+        />
+        <CardList btnDetails={AddBtnDetails} movieArray={movieArray} />
       </div>
     </>
   );
